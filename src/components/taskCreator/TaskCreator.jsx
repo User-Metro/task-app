199220343595d586425b69931003f9ee8eed4739
Abdon <estadia2020@gmail.com>
2022-04-14 18:57:21 -0500

    completed

--- src/components/taskCreator/TaskCreator.jsx
+++ src/components/taskCreator/TaskCreator.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
+import styles from "./TaskCreator.module.css";
 
-export const TaskCreator = props => {
+export const TaskCreator = (props) => {
   const [newTaskName, setNewTaskName] = useState("");
 
-  const updateNewTaskValue = e => setNewTaskName(e.target.value);
+  const updateNewTaskValue = (e) => setNewTaskName(e.target.value);
 
   const createNewTask = () => {
     props.callback(newTaskName);
-    setNewTaskName('');
-  }
+    setNewTaskName("");
+  };
 
   return (
-    <div className="my-1">
+    <div className={styles.container}>
+      <button className={styles.btnTaskCreator} onClick={createNewTask}>
+        Add
+      </button>
       <input
         type="text"
-        className="form-control"
+        placeholder="Agregar una nueva tarea"
+        className={styles.inputTaskCreator}
         value={newTaskName}
         onChange={updateNewTaskValue}
       />
-      <button className="btn btn-primary mt-1" onClick={createNewTask}>
-        Add
-      </button>
     </div>
   );
 };
